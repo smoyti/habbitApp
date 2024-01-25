49f83b51bd314e0101cbf9af4f6e3d27c450b145
Maksym Laktionov <senior.laktion@bmail.com>
2024-01-25 11:31:55 +0200

    Loading and saving data

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,20 @@
-    function say() {
-        alert('Hellio')
+'use strict'
+
+let habbits = [];
+let HABBIT_KEY = 'HABBIT_KEY';
+
+ function loadData() {
+    const habbitsString = localStorage.getItem('HABBIT_KEY');
+    const habbitArray = JSON.parse(habbitsString);
+    if(Array.isArray(habbitArray)) {
+        habbits = habbitArray;
     }
+ }
+
+ function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+ }
+
+ (() => {
+    loadData();
+ }) ()
