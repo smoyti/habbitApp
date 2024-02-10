d372834792070e6e3f9f20e61ef91c63102905ab
Maksym Laktionov <senior.laktion@bmail.com>
2024-02-10 07:49:13 +0200

    Добавление дней

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalActiveHabbitId;
 
 const page = {
 	menu: document.querySelector('.menu__list'),
@@ -79,6 +80,7 @@ function rerenderContent(activeHabbit) {
 
 
 function rerender(activeHabbitId) {
+	globalActiveHabbitId = activeHabbitId;
 	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
 	if(!activeHabbit){
 		return;
@@ -89,9 +91,26 @@ function rerender(activeHabbitId) {
 }
 
 function addDAys(event) {
+	const form = event.target;
 	event.preventDefault();
-	const data = new FormData(event.target);
-	console.log(data.get('comment'));
+	const data = new FormData(form);
+	const comment = data.get('comment');
+	form['comment'].classList.remove('error')
+	if(!comment){
+		form['comment'].classList.add('error')
+	} 
+	habbits = habbits.map(habbit => {
+		if(habbit.id === globalActiveHabbitId) {
+			return {
+				...habbit,
+				days: habbit.days.concat([{ comment }])
+			}
+		}
+		return habbit;
+	});
+	form['comment'].value = '';
+	rerender(globalActiveHabbitId)
+	saveData();
 }
 
 (() => {
